950d667f2d38ea156f8e1fdb5a461de52d64bbc0
Christian Costa Queiroz <christian.queiroz@sicoobdovale.com.br>
2025-01-20 13:41:47 -0300

    Adicionando centenas

--- numeros_por_extenso.py
+++ numeros_por_extenso.py
@@ -1,10 +1,26 @@
-unidades = ["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"]
-especiais = ["dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"]
+unidades_e_especiais = ["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove","dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"]
 dezenas = ["", "", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"]
 centenas = ["", "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"]
+milhares = ["", "mil", "milhão", "bilhão", "trilhão"]
 
 def extenso(numero):
-    return unidades[numero]
+    if numero < 20:
+        return unidades_e_especiais[numero]
+    if numero < 100:
+        dezena = numero // 10
+        unidade = numero % 10
+        texto = dezenas[dezena]
+        if unidade != 0:
+            texto += f" e {extenso(unidade)}"
+        return texto
+    if numero < 1000:
+        centena = numero // 100
+        resto = numero % 100
+        texto = centenas[centena]
+        if resto != 0:
+            texto += f" e {extenso(resto)}"
+        return texto
 
-for i in range(10):
-    print(extenso(i))
+for i in range(100, 201):
+    texto = extenso(i)
+    print(texto, i, len(texto))
