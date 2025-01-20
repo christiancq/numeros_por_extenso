7ebce75019ac30907d2eb49ed2e54e6f8128ccb7
Christian Costa Queiroz <christian.queiroz@sicoobdovale.com.br>
2025-01-20 16:46:29 -0300

    Adiconado função quebrar_numero e extenso

--- numeros_por_extenso.py
+++ numeros_por_extenso.py
@@ -1,9 +1,11 @@
+from math import ceil
+
 unidades_e_especiais = ["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove","dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"]
 dezenas = ["", "", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"]
 centenas = ["", "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"]
 milhares = ["", "mil", "milhão", "bilhão", "trilhão"]
 
-def extenso(numero):
+def extenso999(numero):
     if numero < 20:
         return unidades_e_especiais[numero]
     if numero < 100:
@@ -11,7 +13,7 @@ def extenso(numero):
         unidade = numero % 10
         texto = dezenas[dezena]
         if unidade != 0:
-            texto += f" e {extenso(unidade)}"
+            texto += f" e {extenso999(unidade)}"
         return texto
     if numero < 1000:
         centena = numero // 100
@@ -20,9 +22,28 @@ def extenso(numero):
             return "cem"
         texto = centenas[centena]
         if resto != 0:
-            texto += f" e {extenso(resto)}"
+            texto += f" e {extenso999(resto)}"
         return texto
 
-for i in range(900, 1000):
+def quebar_numero(numero):
+    str_numero = str(numero)
+    tamanho_numero = len(str_numero)
+    quantidade_partes = ceil(tamanho_numero / 3)
+    posicao_inicial = 0
+    posicao_final = tamanho_numero % 3 if tamanho_numero % 3 != 0 else 3
+    partes_numero = []
+    for _ in range(quantidade_partes):
+        partes_numero.append(int(str_numero[posicao_inicial:posicao_final]))
+        posicao_inicial = posicao_final
+        posicao_final += 3
+    return partes_numero
+
+def extenso(numero):
+    texto = ""
+    for parte in quebar_numero(numero):
+        texto += extenso999(parte)
+    return texto
+
+for i in range(1000, 1002):
     texto = extenso(i)
     print(texto, i, len(texto))
