66fdfb8c93d145be96b68b55c44800dd7f0af221
Christian Costa Queiroz <christian.queiroz@sicoobdovale.com.br>
2025-01-20 10:50:51 -0300

    Adicionando vetores de palavras

--- numeros_por_extenso.py
+++ numeros_por_extenso.py
@@ -1,3 +1,10 @@
+unidades = ["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"]
+especiais = ["dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"]
+dezenas = ["", "", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"]
+centenas = ["", "cento", "duzentos", "trezentos", "quatrocentos", "quinhentos", "seiscentos", "setecentos", "oitocentos", "novecentos"]
+
 def extenso(numero):
-    extenso = ""
-    return extenso
+    return unidades[numero]
+
+for i in range(10):
+    print(extenso(i))
